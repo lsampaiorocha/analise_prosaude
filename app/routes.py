a387d7b2f19fe7f3256e6ab18b302453ec20f4bf
lsampaiorocha <lsampaiorocha@gmail.com>
2024-10-22 16:02:50 -0300

    Versao 1.3.5. Correção da falta de valores de retorno nas rotas

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def configure_routes(app):
     Rota para importar os processos de intimações cujos autos já tenham sido baixados pelos
     robô de distribuição de processos (tabela tb_autosprocessos) para a tabela tb_analiseportaria
     """ 
-    importar_processos()
+    return importar_processos()
   
 
   @app.route('/AnalisarProcessosMarcados', methods=['POST'])
@@ -33,34 +33,34 @@ def configure_routes(app):
         - possuam o campo id_documento definido
     """ 
     
-    analisar_marcados()
+    return analisar_marcados()
 
     
     
     
   @app.route('/AnalisePortariaProcesso', methods=['POST'])
   def analise_processo():
-        """
-        Rota de análise de aplicação de portaria para um processo.
-        A rota recebe um número de processo como parâmetro e executa diversas etapas necessárias para
-        análise, desde a recuperação dos autos no Alfresco, até a extração do documento a ser analisado
-        e aplicação do pipeline de análise de aplicação de portaria
-        
-        Parâmetros:
-        - numero_processo (form): Número do processo a ser analisado.
-        
-        Fluxo:
-        1. Validação do número de processo e mesmo foi marcado para análise.
-        2. Download dos autos completos do processo (Alfresco).
-        3. Processamento dos autos para extrair o documento a ser analisado
-        4. Execução do pipeline de Análise.
-        5. Escrita dos resultados da análise no BD.
-        """    
-        
-        numero_processo = request.form.get('numero_processo')
-        #TODO: Verificar casos em que o número não tenha sido passado no formato padrão
-        
-        analisar_processo(numero_processo)
+    """
+    Rota de análise de aplicação de portaria para um processo.
+    A rota recebe um número de processo como parâmetro e executa diversas etapas necessárias para
+    análise, desde a recuperação dos autos no Alfresco, até a extração do documento a ser analisado
+    e aplicação do pipeline de análise de aplicação de portaria
+    
+    Parâmetros:
+    - numero_processo (form): Número do processo a ser analisado.
+    
+    Fluxo:
+    1. Validação do número de processo e mesmo foi marcado para análise.
+    2. Download dos autos completos do processo (Alfresco).
+    3. Processamento dos autos para extrair o documento a ser analisado
+    4. Execução do pipeline de Análise.
+    5. Escrita dos resultados da análise no BD.
+    """    
+    
+    numero_processo = request.form.get('numero_processo')
+    #TODO: Verificar casos em que o número não tenha sido passado no formato padrão
+    
+    return analisar_processo(numero_processo)
 
     
   @app.route('/CapturaIdsProcessos', methods=['POST'])
